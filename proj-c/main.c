66ae2bf454974c43cd3d07f16fbb8f092a668589
Hungu Lim <imoofs@gmail.com>
2022-01-31 22:27:11 +0900

    Initial Commit

---
+++ proj-c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  proj-c
+//
+//  Created by Hungu Lim on 2022/01/31.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
